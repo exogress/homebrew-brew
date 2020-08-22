df1cdb8029738b45ba172dff76d4879c06ee5413
Gleb Pomykalov <gleb@exogress.com>
2020-08-23 00:59:58 +0300

    0.0.1-alpha.1

--- exogress.rb
+++ exogress.rb
@@ -3,11 +3,11 @@ class Exogress < Formula
   homepage "https://github.com/exogress/exogress"
   version "0.0.1-alpha.1"
   url "https://github.com/exogress/exogress/releases/download/v0.0.1-alpha.1/exogress-v0.0.1-alpha.1-x86_64-apple-darwin.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "144209ec1789900c98b0fa1f20f8141b705ea84ada51fd96eabe1065c9c6b794"
 
   if OS.linux?
     url "https://github.com/exogress/exogress/releases/download/v0.0.1-alpha.1/exogress-v0.0.1-alpha.1-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "1983080a6e322651f86e6d9f6874f8b24b18dcb51b6330d41dfaf1e3106da118"
   end
 
   def install
