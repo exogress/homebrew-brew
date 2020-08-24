a2f8c3467a9246adcc04f3328a13f42e9b5505c9
Package Publisher <team@exogress.com>
2020-08-24 12:29:45 +0000

    0.0.1-alpha.25: Release new version

--- exogress.rb
+++ exogress.rb
@@ -1,13 +1,13 @@
 class Exogress < Formula
   desc "Exogress command-line client"
   homepage "https://www.exogress.com/"
-  version "0.0.1-alpha.24"
-  url "https://github.com/exogress/exogress/releases/download/0.0.1-alpha.24/exogress-0.0.1-alpha.24-x86_64-apple-darwin.tar.gz"
-  sha256 "93985435ccafb138d63b40c8e1633255220a13b0e94309af2242879db51dd24d"
+  version "0.0.1-alpha.25"
+  url "https://github.com/exogress/exogress/releases/download/0.0.1-alpha.25/exogress-0.0.1-alpha.25-x86_64-apple-darwin.tar.gz"
+  sha256 "b6c1dc5b411068c86ebfe951568916f7ee0eb1231bd66899286b358699d47678"
 
   if OS.linux?
-    url "https://github.com/exogress/exogress/releases/download/0.0.1-alpha.24/exogress-0.0.1-alpha.24-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "c45f3b84168b82590c7c3d7804107a27d7badac8bdc1c552482778671fa5579c"
+    url "https://github.com/exogress/exogress/releases/download/0.0.1-alpha.25/exogress-0.0.1-alpha.25-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "e1111a7063e24808fedccffacf9ffc8b5d7493b69a4f4a38168b59a314df9d16"
   end
 
   def install
