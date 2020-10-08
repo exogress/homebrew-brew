b154088c437294a373fe0d5768e1902cd91227e3
Package Publisher <team@exogress.com>
2020-10-08 19:04:23 +0000

    0.0.1-alpha.47: Release new version

--- exogress.rb
+++ exogress.rb
@@ -3,11 +3,11 @@ class Exogress < Formula
   homepage "https://www.exogress.com/"
   version "0.0.1-alpha.47"
   url "https://github.com/exogress/cli/releases/download/v0.0.1-alpha.47/exogress-macos-x86_64.tar.gz"
-  sha256 "aab299ec29c318b36bada9970614647cd5ee0f3ee061ebed62343e1715179b44"
+  sha256 "99ec3294c41b203a0c4279135b2a38e6e22b9de2c1e35caf947fb0fba38252d1"
 
   if OS.linux?
     url "https://github.com/exogress/cli/releases/download/v0.0.1-alpha.47/exogress-linux-x86_64-musl-static.tar.gz"
-    sha256 "b6afecbc8fe4d6cb8ab7ab387eca8d71fd57537393b453f5440f5b1792b8a151"
+    sha256 "bba1ea4a3c39963a7aee0daeef852cac323e966c520ad3b8621ac7f87b7750a1"
   end
 
   def install
