5f72794385bf21cec45edf7693c067329ab9d422
Package Publisher <team@exogress.com>
2021-02-17 01:44:54 +0000

    0.0.1-beta.25: Release new version

--- exogress.rb
+++ exogress.rb
@@ -3,11 +3,11 @@ class Exogress < Formula
   homepage "https://www.exogress.com/"
   version "0.0.1-beta.25"
   url "https://github.com/exogress/cli/releases/download/v0.0.1-beta.25/exogress-macos-x86_64.tar.gz"
-  sha256 "e8db4b1534561fd549ff84cf7bb3e5074b477c46c8a6122c9ff474c64cb08194"
+  sha256 "f13e3dbc1de582d715ca33e88d75d4d60a8195fe85e9961cdda4b7a45da79329"
 
   if OS.linux?
     url "https://github.com/exogress/cli/releases/download/v0.0.1-beta.25/exogress-linux-x86_64-musl-static.tar.gz"
-    sha256 "3866d054a07462132ab654653e79084b6291824b077c1908e960f20886211628"
+    sha256 "295404fa8791d2eb2ebea00f55b31e867adb2cfba4b82094ff325f2aa8fd9b0e"
   end
 
   def install
