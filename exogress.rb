a1f0a514f578cd494d888892812d16ab6187ff91
Package Publisher <team@exogress.com>
2021-03-07 12:18:34 +0000

    0.1.0-pre.4: Release new version

--- exogress.rb
+++ exogress.rb
@@ -1,13 +1,13 @@
 class Exogress < Formula
   desc "Exogress command-line client"
   homepage "https://www.exogress.com/"
-  version "0.1.0-pre.3"
-  url "https://github.com/exogress/cli/releases/download/v0.1.0-pre.3/exogress-macos-x86_64.tar.gz"
-  sha256 "102b54d6c6322abe76bbd840677b1f29897ac37525187fca184d8e5e1fb23bf0"
+  version "0.1.0-pre.4"
+  url "https://github.com/exogress/cli/releases/download/v0.1.0-pre.4/exogress-macos-x86_64.tar.gz"
+  sha256 "d2d03df4181ca08459c581c9258fb4e347b398854dc264d1ae85f4eddb18619e"
 
   if OS.linux?
-    url "https://github.com/exogress/cli/releases/download/v0.1.0-pre.3/exogress-linux-x86_64-musl-static.tar.gz"
-    sha256 "54110020060967ef3496a1ba4963facbb3279892a9ab5b40853d4d99278087cb"
+    url "https://github.com/exogress/cli/releases/download/v0.1.0-pre.4/exogress-linux-x86_64-musl-static.tar.gz"
+    sha256 "eed5f1b672f19bc52b998e2b3afa1d598fd486ead76e2da26c3a1a47f95d1405"
   end
 
   def install
