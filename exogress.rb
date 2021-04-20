35efe766849a7569fa0d44ee313647a1ad9507df
Gleb Pomykalov <gleb@exogress.com>
2021-04-20 13:20:51 +0300

    try install through rust

--- exogress.rb
+++ exogress.rb
@@ -1,16 +1,14 @@
 class Exogress < Formula
   desc "Exogress command-line client"
   homepage "https://www.exogress.com/"
-  version "0.2.0"
-  url "https://github.com/exogress/cli/releases/download/v0.2.0/exogress-macos-x86_64.tar.gz"
-  sha256 "c5f0973dab076f78f226e93ee6b263deeb3c3e55f756f136c627c0897820c91b"
+  url "https://github.com/exogress/cli/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "b6ae6099c1e2acd702d87cb94b067328d726e08e903e6e6ff7871709eae74d7f"
+  license "Apache-2.0"
 
-  if OS.linux?
-    url "https://github.com/exogress/cli/releases/download/v0.2.0/exogress-linux-x86_64-musl-static.tar.gz"
-    sha256 "6f5ce3018dd981ea050c5ee663baf11f118a31b0e69f55dd88aebe52d31d04fa"
-  end
+  depends_on "rust" => :build
 
   def install
-    bin.install "exogress"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cargo", "install", *std_cargo_args
   end
 end
