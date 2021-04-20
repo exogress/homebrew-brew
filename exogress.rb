092f4097869616d529fc735a963dd2318c077619
Gleb Pomykalov <gleb@exogress.com>
2021-04-20 13:23:57 +0300

    0.2.1

--- exogress.rb
+++ exogress.rb
@@ -1,14 +1,13 @@
 class Exogress < Formula
   desc "Exogress command-line client"
   homepage "https://www.exogress.com/"
-  url "https://github.com/exogress/cli/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "b6ae6099c1e2acd702d87cb94b067328d726e08e903e6e6ff7871709eae74d7f"
+  url "https://github.com/exogress/cli/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "9f7a5933a4c1c78abf99fff93907d9d8d3d6cfd57a2f7ef32affea67500ee383"
   license "Apache-2.0"
 
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", *std_cargo_args
   end
 end
