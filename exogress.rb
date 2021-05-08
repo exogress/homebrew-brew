920a73922c989a8b18f5207fb0d55c24757e8e47
Package Publisher <team@exogress.com>
2021-05-08 17:20:05 +0000

    0.4.0: Release new version

--- exogress.rb
+++ exogress.rb
@@ -1,9 +1,9 @@
 class Exogress < Formula
   desc "Exogress command-line client"
   homepage "https://www.exogress.com/"
-  version "0.3.0"
-  url "https://github.com/exogress/cli/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "94912a0d64631752b391e13de34b1f95ec1ce4da7a69e7862fecec0712a7ae38"
+  version "0.4.0"
+  url "https://github.com/exogress/cli/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "3dd363b11d96077034753c4893d55e2ec135c30f7da958410d0e05a04b2b7c2c"
   license "Apache-2.0"
 
   depends_on "rust" => :build
